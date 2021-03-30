296d6283a3d63c5fbac46d313474bd710f35d81e
vadim <vadim.tatochko98@mail.ru>
2021-03-30 23:48:58 +0300

    page 2: roll

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,32 @@
-$(function(){
+jQuery.easing['easeOutCirc'] = function (x, t, b, c, d) {
+    return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+}
+$(function() {
+var option = {
+    speed: 6,
+    duration: 3,
+    stopImageNumber: 4
+};
 
-
-
-  
+$("#go").click(function() {
+$('#go').prop('disabled', true);
+option.stopImageNumber = Math.random() * 11|0;
+$(".roulette-inner  img").removeClass("active");
+$("#config").text(JSON.stringify(option));
+$({
+    left: 0
+}).animate({
+    left: 844 * option.speed + 75 * (option.stopImageNumber + 6)
+}, {
+    duration: option.duration * 1000,
+    easing: "easeOutCirc",
+    step: function(a) {
+        $(".roulette-inner").css("transform", "translateX(-" + a % 844 + "px)");
+    },
+    complete: function() {
+        $(".roulette-inner  img").eq(option.stopImageNumber + (option.stopImageNumber < 6) * 11).addClass("active");
+        $('#go').prop('disabled', false);
+    }
+});
+})
 });
